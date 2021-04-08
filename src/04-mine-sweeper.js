d47a1bb995315481a509c410f58c89c1c4946c6e
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-04-08 10:42:57 -0300

    feat: add function 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,80 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixCopy = new Array(matrix.length);
+
+  for (let i = 0; i < matrixCopy.length; i++) {
+    matrixCopy[i] = new Array(matrix[0].length);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrixCopy[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === true) {
+        if (i === 0
+          && j === 0) {
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (i === 0
+          && j === matrixCopy[0].length - 1) {
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+        } else if (i === matrixCopy.length - 1
+          && j === 0) {
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j + 1]++;
+        } else if (
+          i === matrixCopy.length - 1
+          && j === matrixCopy[0].length - 1
+        ) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i][j - 1]++;
+        } else if (i === 0) {
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (i === matrixCopy.length - 1) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+        } else if (j === 0) {
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (j === matrixCopy[0].length - 1) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+        } else {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        }
+      }
+    }
+  }
+  return matrixCopy;
 }
 
 module.exports = minesweeper;
